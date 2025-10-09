a8fb1da1c5ddca2b90ff29ca7ca1c4e71657c101
Gauri Shirke <gaurishirke.2005@gmail.com>
2025-10-09 19:14:53 +0530

    added files

--- utils/wrapAsync.js
+++ utils/wrapAsync.js
@@ -1,7 +1,8 @@
 function wrapAsync(fn) {
   return function(req, res, next) {
-    fn(req, res, next).catch(next);
-  }
+    // Ensure both sync and async handlers are supported
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
 }
 
 module.exports = wrapAsync;
